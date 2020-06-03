9da7efb3bf2255e6d91028b2ec0ca7f3dd358876
Zhenya Barsamov <46850036+ZhenyaBarsamov@users.noreply.github.com>
2020-06-03 17:49:37 +0500

    Добавлена библиотека с интерфейсами плагинов

---
+++ PaintInterfaces/Interfaces.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintInterfaces
+{
+    /// <summary>
+    /// Общий интерфейс для плагинов
+    /// </summary>
+    public interface IPlugin {
+        /// <summary>
+        /// Название плагина
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Версия плагина
+        /// </summary>
+        string Version { get; }
+
+        /// <summary>
+        /// Имя автора плагина
+        /// </summary>
+        string Author { get; }
+    }
+
+    /// <summary>
+    /// Интерфейс для фильтров-плагинов
+    /// </summary>
+    public interface IFilterPlugin : IPlugin {
+        /// <summary>
+        /// Применяет фильтр к изображению
+        /// </summary>
+        /// <param name="image">Изображение, подлежащее изменению</param>
+        Bitmap TransformImage(Bitmap image);
+    }
+
+    /// <summary>
+    /// Интерфейс для фигур-плагинов
+    /// </summary>
+    public interface IFigurePlugin : IPlugin {
+        // TODO: Как программист узнает, какие параметры передавать в fFarams?
+
+        /// <summary>
+        /// Рисует фигуру по заданным параметрам
+        /// </summary>
+        /// <param name="gCanvas">Поверхность, на которой будет нарисована фигура</param>
+        /// <param name="pen">Перо для рисования</param>
+        /// <param name="xLeft">Координата X левого верхнего угла рисунка</param>
+        /// <param name="yTop">Координата Y левого верхнего угла рисунка</param>
+        /// <param name="xRight">Координата X правого нижнего угла рисунка</param>
+        /// <param name="yBottom">Координата Y правого нижнего угла рисунка</param>
+        /// <param name="isFilled">Необходимо ли закрасить нарисованную фигуру</param>
+        /// <param name="fParams">Список параметров фигуры</param>
+        void DrawFigure(Graphics gCanvas, Pen pen, float xLeft, float yTop, float xRight, float yBottom, bool isFilled, params float[] fParams);
+    }
+}
